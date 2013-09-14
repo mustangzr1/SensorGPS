fa7003e9eb2a75a54e8523fe2dba63b22ff27ad2
Oliver <Oliver@192.168.0.10>
2013-09-13 20:38:06 -0700

    a

--- SensorGPS/src/com/comp595/sensorgps/MainActivity.java
+++ SensorGPS/src/com/comp595/sensorgps/MainActivity.java
@@ -10,16 +10,17 @@ import android.view.Menu;
 import android.widget.TextView;
 
 public class MainActivity extends Activity implements SensorEventListener {
-	
+
 	SensorManager manager;
-	TextView tv1;
+	TextView tv1, tv2;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
 
+		tv1 = (TextView) findViewById(R.id.textView1);
+		tv2 = (TextView) findViewById(R.id.textView2);
 	}
 
 	@Override
